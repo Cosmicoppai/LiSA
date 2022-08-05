16ee85b03fd4fe93435708d0d81e7e4ea0ffbc23
cosmicoppai <kanakchaudhari12@gmail.com>
2022-08-06 05:28:02 +0530

    revert the server addresses

--- scraper/main.py
+++ scraper/main.py
@@ -18,12 +18,12 @@ def run_api_server(port: int = 8000):
 if __name__ == "__main__":
     try:
         logging.basicConfig(stream=stdout, level=logging.ERROR)
-        p1 = Thread(target=run_api_server, args=(9001, ))
+        p1 = Thread(target=run_api_server, args=(6969, ))
         p1.daemon = True
         p1.start()
         event_loop = asyncio.new_event_loop()  # get new event loop
         asyncio.set_event_loop(event_loop)
-        msg_system = MsgSystem(9999)
+        msg_system = MsgSystem()
         msg_system.event_loop = event_loop  # assign event loop
         event_loop.run_until_complete(msg_system.run_server())  # run socket server
     except KeyboardInterrupt:
