7bf89d842e540973db1713c7f0992a593cd4d2a4
NishantChaware <ndchaware99@gmail.com>
2022-08-05 09:26:45 +0530

    initialize ui

---
+++ ui/public/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+const isDev = require('electron-is-dev')
+
+require('@electron/remote/main').initialize()
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    }
+  })
+
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  )
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
---
+++ ui/src/App.js
@@ -0,0 +1,8 @@
+import { useState, useMemo } from 'react'
+
+
+function App() {
+  return <h1>asdas</h1>
+}
+
+export default App
