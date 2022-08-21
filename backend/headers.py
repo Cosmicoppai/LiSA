5fb6c7b627163e345bbebdb19eac7b1ed5d4ced5
cosmicoppai <kanakchaudhari12@gmail.com>
2022-08-21 15:08:17 +0530

    assign default value to param

--- backend/headers.py
+++ backend/headers.py
@@ -1,7 +1,7 @@
 from typing import Dict, List, Any
 
 
-def get_headers(extra: Dict[str, Any]) -> Dict[str, str]:
+def get_headers(extra: Dict[str, Any] = {}) -> Dict[str, str]:
     headers = {
         "accept-language": "en-GB,en;q=0.9,ja-JP;q=0.8,ja;q=0.7,en-US;q=0.6",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
