764ee4309e43b90ae05540a00f591665fd664a3d
cosmicoppai <kanakchaudhari12@gmail.com>
2022-08-23 00:41:28 +0530

    update headers

--- backend/headers.py
+++ backend/headers.py
@@ -3,6 +3,7 @@ from typing import Dict, List, Any
 
 def get_headers(extra: Dict[str, Any] = {}) -> Dict[str, str]:
     headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "accept-language": "en-GB,en;q=0.9,ja-JP;q=0.8,ja;q=0.7,en-US;q=0.6",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
     }
