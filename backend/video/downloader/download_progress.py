c7696dca525fe509dc9a55e17348fcf1230dad90
cosmicoppai <kanakchaudhari12@gmail.com>
2022-09-19 22:54:20 +0530

    rm download_progress.py

--- backend/video/downloader/download_progress.py
+++
@@ -1,5 +0,0 @@
-from typing import Dict, List, Any
-
-IN_PROGRESS: Dict[str, Any] = {}
-
-LAST_PACKET_DATA: Dict[str, int] = {}  # {"file_name" : [amount_of_data_downloaded_last_time, time_at_which_packet_was_downloaded]}
