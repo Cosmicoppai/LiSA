f10b12c24a1a3e6d36309347550492e4e540e65a
NishantChaware <ndchaware99@gmail.com>
2022-11-14 09:04:58 +0530

    change folder structure

--- ui/src/socket.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-var W3CWebSocket = require("websocket").w3cwebsocket;
-
-export const client = new W3CWebSocket("ws://localhost:9000");
-
-export const SocketContext = React.createContext();
