a67908987960b4ac333d4be2ae06bbf1f5c3f3cf
cosmicoppai <kanakchaudhari12@gmail.com>
2022-11-19 16:50:01 +0530

    rm setup scripts and db

--- backend/scripts/setup.ps1
+++
@@ -1,5 +0,0 @@
-function Add-Path([string]$Path) {
-    $Env:Path += [IO.Path]::PathSeparator + $Path
-    $command = "[Environment]::SetEnvironmentVariable('PATH', '$Env:Path', 'Machine')"
-    Start-Process powershell -ArgumentList "-command $Command" -Verb runas
-}
