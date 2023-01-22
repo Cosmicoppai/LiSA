5eefb0d26da779118b184e2333a82920494a3a99
cosmicoppai <kanakchaudhari12@gmail.com>
2023-01-23 02:46:23 +0530

    add type and series_name

--- backend/sql_queries/progress_tracker.sql
+++ backend/sql_queries/progress_tracker.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS progress_tracker (
     id integer PRIMARY KEY,
+    type varchar NOT NULL DEFAULT 'video',  -- can be of type video or image
+    series_name varchar NOT NULL ,
     file_name varchar NOT NULL UNIQUE,
     status char(50) NOT NULL,
     total_size int NOT NULL,
