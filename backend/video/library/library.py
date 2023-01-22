85dd7de0c1d8991e3c5711ba8c72da6b1343035d
cosmicoppai <kanakchaudhari12@gmail.com>
2023-01-23 02:47:52 +0530

    add WatchList

--- backend/video/library/library.py
+++ backend/video/library/library.py
@@ -5,6 +5,8 @@ This file will handle the saving and extraction of metadata about downloaded fil
 Example: {file_name: {total_size: int, location: str}}
 
 """
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from typing import Dict, List, Any
 import json
@@ -14,45 +16,60 @@ from sqlite3 import IntegrityError
 
 
 class Library(ABC):
-    data: Dict[int, Dict[str, Any]]
+    data: Dict[int, Dict[str, Any]] = {}
+    _libraries: List[Library] = []
+    fields: str = ""
+    oid: str = "id"
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls._libraries.append(cls)
 
     @classmethod
-    @abstractmethod
-    def get_all(cls) -> List[Dict[str, Dict[str, Any]]]:
-        ...
+    def load_datas(cls):
+        for lib in cls._libraries:
+            lib.load_data()
 
     @classmethod
-    @abstractmethod
-    def create(cls, _data: Dict[str, Dict[str, Any]]) -> None:
-        ...
+    def update(cls, _id: int, data: Dict[str, Any]) -> None:
+        set_statement, field_values = cls.__query_builder(data, "update")
+        field_values.append(_id)
+        cmd = f"UPDATE {cls.table_name} SET {set_statement} WHERE {cls.oid}=?"
+        cur = DB.connection.cursor()
+        cur.execute(cmd, field_values)
+        DB.connection.commit()
+        cur.execute(f"SELECT {cls.fields} from {cls.table_name} WHERE {cls.oid}=?;", [_id, ])
+        cls.data[_id] = dict(cur.fetchone())
+        cur.close()
 
     @classmethod
-    @abstractmethod
-    def update(cls, _id: int, _data: Dict[str, Dict[str, Any]]) -> None:
-        ...
+    def load_data(cls) -> None:
 
-    @classmethod
-    @abstractmethod
-    def delete(cls, _id: int) -> None:
-        ...
-
-    @classmethod
-    @abstractmethod
-    def load_data(cls):
-        ...
-
-
-class DBLibrary(Library):
-    table_name: str = "progress_tracker"
+        """
+        Load all data from database
+        """
+        cur = DB.connection.cursor()
+        cur.execute(f"SELECT {cls.fields} from {cls.table_name};")
+        for row in cur.fetchall():
+            data = dict(row)
+            cls.data[data[cls.oid]] = data
+        cur.close()
 
     @classmethod
     def get_all(cls) -> List[Dict[int, Dict[str, Any]]]:
         return [data for data in cls.data.values()]
 
     @classmethod
-    def get(cls, filters: Dict[str, Any]) -> List[Dict[str, Any]]:
+    def get(cls, filters: Dict[str, Any], query: List[str] = ("*",)) -> List[Dict[str, Any]]:
         cur = DB.connection.cursor()
-        cmd = f"SELECT * FROM {cls.table_name} WHERE "
+
+        _query: str = ""
+        for idx, _queri in enumerate(query):
+            if idx != 0:
+                _query += ","
+            _query += _queri
+
+        cmd = f"SELECT {_query} FROM {cls.table_name} WHERE "
         for idx, _filter in enumerate(filters):
             if idx != 0:
                 cmd += "AND "
@@ -66,34 +83,22 @@ class DBLibrary(Library):
     @classmethod
     def create(cls, data: Dict[str, Any]) -> None:
         set_statement, field_values = cls.__query_builder(data)
-        cmd = f"INSERT INTO {cls.table_name} ({set_statement}) VALUES {'(' + ','.join('?' * len(data))+ ')'}"
+        cmd = f"INSERT INTO {cls.table_name} ({set_statement}) VALUES {'(' + ','.join('?' * len(data)) + ')'}"
         try:
             cur = DB.connection.cursor()
             cur.execute(cmd, field_values)
             DB.connection.commit()
             cur.close()
         except IntegrityError:
-            raise ValueError("File with this name already exist")
+            raise ValueError("Record already exist")
 
         cls.data[data["id"]] = data
-
-    @classmethod
-    def update(cls, _id: int, data: Dict[str, Any]) -> None:
-        set_statement, field_values = cls.__query_builder(data, "update")
-        field_values.append(_id)
-        cmd = f"UPDATE {cls.table_name} SET {set_statement} WHERE id=?"
-        cur = DB.connection.cursor()
-        cur.execute(cmd, field_values)
-        DB.connection.commit()
-        cur.execute(f"SELECT id, file_name, status, created_on, total_size, file_location from {cls.table_name} WHERE id=?;", [_id, ])
-        cls.data[_id] = dict(cur.fetchone())
-        cur.close()
 
     @classmethod
     def delete(cls, _id: int) -> None:
         del cls.data[_id]
         cur = DB.connection.cursor()
-        cur.execute(f"DELETE FROM {cls.table_name} WHERE id=?", [_id, ])
+        cur.execute(f"DELETE FROM {cls.table_name} WHERE {cls.oid}=?", [_id, ])
         DB.connection.commit()
         cur.close()
 
@@ -110,16 +115,14 @@ class DBLibrary(Library):
         set_statement = ", ".join(fields_to_set)
         return set_statement, field_values
 
-    @classmethod
-    def load_data(cls) -> None:
 
-        """
-        Load all data from database
-        if status is in progress eventual updates will be sent through socket server
-        """
-        cur = DB.connection.cursor()
-        cur.execute(f"SELECT id, file_name, status, created_on, total_size, file_location from {cls.table_name};")
-        for row in cur.fetchall():
-            data = dict(row)
-            cls.data[data["id"]] = data
-        cur.close()
+class DBLibrary(Library):
+    table_name: str = "progress_tracker"
+    fields: str = "id, type, series_name, file_name, status, created_on, total_size, file_location"
+    oid: str = "id"
+
+
+class WatchList(Library):
+    table_name: str = "watchlist"
+    fields: str = "anime_id, jp_name, no_of_episodes, type, status, season, year, score, ep_details, created_on"
+    oid: str = "anime_id"
