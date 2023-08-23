9f895bba35024554c633d894a71e6a52a725e204
cosmicoppai <kanakchaudhari12@gmail.com>
2023-08-23 20:45:29 +0530

    added port parser func

--- backend/LiSA.py
+++ backend/LiSA.py
@@ -14,10 +14,20 @@ from scraper import Scraper
 
 def run_api_server(port: int = 8000):
     ServerConfig.API_SERVER_ADDRESS = f"http://localhost:{port}"
-    print(
-        f"server started on port: {port} \n You can access API SERVER on {ServerConfig.API_SERVER_ADDRESS}"
-    )
+    print(f"server started on port: {port} \n You can access API SERVER on {ServerConfig.API_SERVER_ADDRESS}")
     start_api_server(port=port)
+
+
+def get_ports():
+    _api_port, _web_sock_port = 6969, 9000
+    try:
+        if len(argv) >= 3:
+            _api_port, _web_sock_port = int(argv[1]), int(argv[2])
+        elif len(argv) >= 2:
+            _api_port = int(argv[1])
+    except ValueError:
+        ...
+    return _api_port, _web_sock_port
 
 
 if __name__ == "__main__":
@@ -36,10 +46,9 @@ if __name__ == "__main__":
         parse_config_json(FileConfig.CONFIG_JSON_PATH)
         update_environ()
 
-        # For your development cosmic, you don't need to change your testing port number
-        PORT = int(argv[1]) if (len(argv) >= 2 and argv[1]) else 6969
+        api_port, web_sock_port = get_ports()
 
-        t1 = Thread(target=run_api_server, args=(PORT,))
+        t1 = Thread(target=run_api_server, args=(api_port,))
         t1.daemon = True
         t1.start()
 
@@ -51,7 +60,7 @@ if __name__ == "__main__":
         DownloadManager()
 
         MsgSystem.out_pipe, MsgSystem.in_pipe = Pipe()
-        msg_system = MsgSystem()
+        msg_system = MsgSystem(web_sock_port)
         loop.create_task(msg_system.send_updates())
 
         loop.run_until_complete(msg_system.run_server())  # run socket server
