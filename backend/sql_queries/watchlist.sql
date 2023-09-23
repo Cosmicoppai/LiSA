7c90127e16def5bfcb2435af28a1f3f0b520e06e
CosmicOppai <cosmicoppai@protonmail.com>
2023-09-23 16:15:31 +0530

    update watchlist schema

--- backend/sql_queries/watchlist.sql
+++ backend/sql_queries/watchlist.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS watchlist (
     anime_id integer PRIMARY KEY ,
     jp_name varchar NOT NULL UNIQUE ,
     no_of_episodes NOT NULL CHECK ( no_of_episodes > 0 ),
-    type char(25) NOT NULL UNIQUE ,
+    type char(25) NOT NULL ,
     status char(50) NOT NULL,  -- airing status of anime
     season char(50) NOT NULL ,
     year integer NOT NULL CHECK ( year >= 1900 ),
-    score integer NOT NULL CHECK ( score >= 0 ),    poster varchar NOT NULL,
-    ep_details varchar NOT NULL UNIQUE ,
+    score integer NOT NULL CHECK ( score >= 0 ),
+    poster varchar NOT NULL,
     created_on datetime NOT NULL DEFAULT (datetime('now', 'localtime'))
 );
