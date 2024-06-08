af7550510a565a46d42729f78794b6f4d31c34ae
CosmicOppai <cosmicoppai@protonmail.com>
2024-06-08 22:24:20 +0530

    fix path issue

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -2,3 +2,4 @@ from .init_db import DB
 from .cleanup import remove_folder, remove_file
 from .path_validator import validate_path
 from .staticfiles import CustomStaticFiles
+from .get_path import get_path
