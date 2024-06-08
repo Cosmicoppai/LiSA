45ca1261739efa3691ce829550de870f9baffa19
Rakesh Merugu <mrakesh0608@outlook.com>
2024-06-08 20:20:31 +0530

    fix: reading send of undefined issue

--- src/hooks/useHandleInitialSocketConnection.tsx
+++ src/hooks/useHandleInitialSocketConnection.tsx
@@ -1,35 +1,39 @@
-import { useCallback, useEffect } from "react";
-import { useSocketContext } from "../context/socket";
-import { TCookieReq } from "../types";
+import { useCallback, useEffect } from 'react';
+
+import { useSocketContext } from '../context/socket';
+import { TCookieReq } from '../types';
 
 export function useHandleInitialSocketConnection() {
     const { socket, isSocketConnected } = useSocketContext();
 
-    const handleSocketConnection = useCallback((ev: MessageEvent<any>) => {
-        const msg: TCookieReq = typeof ev?.data === "string" ? JSON.parse(ev?.data) : null;
+    const handleSocketConnection = useCallback(
+        (ev: MessageEvent<any>) => {
+            const msg: TCookieReq = typeof ev?.data === 'string' ? JSON.parse(ev?.data) : null;
 
-        console.log("socket event", msg);
-        if (msg?.data?.type === "cookie_request") {
-            // @ts-ignore
-            window?.electronAPI?.getDomainCookies(msg).then((response) => {
-                socket.send(
-                    JSON.stringify({
-                        type: "cookie_request",
-                        data: response,
-                    })
-                );
-            });
-        }
-    }, []);
+            console.log('socket event', msg);
+            if (msg?.data?.type === 'cookie_request') {
+                // @ts-ignore
+                window?.electronAPI?.getDomainCookies(msg).then((response) => {
+                    socket.send(
+                        JSON.stringify({
+                            type: 'cookie_request',
+                            data: response,
+                        }),
+                    );
+                });
+            }
+        },
+        [socket, window],
+    );
 
     useEffect(() => {
         if (!socket || !isSocketConnected) return;
 
-        socket.send(JSON.stringify({ type: "connect" }));
-        console.log("WebSocket Client Connected");
+        socket.send(JSON.stringify({ type: 'connect' }));
+        console.log('WebSocket Client Connected');
 
-        socket.addEventListener("message", handleSocketConnection);
+        socket.addEventListener('message', handleSocketConnection);
 
-        return () => socket.removeEventListener("message", handleSocketConnection);
-    }, [isSocketConnected, socket, window]);
+        return () => socket.removeEventListener('message', handleSocketConnection);
+    }, [isSocketConnected, socket, handleSocketConnection]);
 }
