48ddc8db42a4e19b8e4a2eea7b018a58adb9992f
CosmicOppai <cosmicoppai@protonmail.com>
2025-07-04 23:40:44 +0530

    feat: adedd fallback for env

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,6 @@
 export const envVariables = {
-    SERVER_URL: import.meta.env.VITE_SERVER_URL,
-    SOCKET_URL: import.meta.env.VITE_SOCKET_URL,
+    SERVER_URL: import.meta.env.VITE_SERVER_URL || "http://localhost:6969",
+    SOCKET_URL: import.meta.env.VITE_SOCKET_URL || "ws://localhost:9000",
 } as const;
 
 export const isViteDEV = import.meta.env.DEV;
