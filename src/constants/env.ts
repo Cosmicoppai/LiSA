1d99910272e4015ede7d2109bd466ce1f3290cc9
CosmicOppai <cosmicoppai@protonmail.com>
2025-07-05 12:50:50 +0530

    refactor: added type hints

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite/client" />
+
 export const envVariables = {
     SERVER_URL: import.meta.env.VITE_SERVER_URL || "http://localhost:9696",
     SOCKET_URL: import.meta.env.VITE_SOCKET_URL || "ws://localhost:9000",
